27d3e49cfb8c266b86fa7f19a0a3778039912a48
divanov11 <ivanovsin11@gmail.com>
2022-01-06 10:43:30 -0500

    first commit

---
+++ base/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import random
+import time
+from agora_token_builder import RtcTokenBuilder
+from .models import RoomMember
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+
+
+# Create your views here.
+
+def lobby(request):
+    return render(request, 'base/lobby.html')
+
+def room(request):
+    return render(request, 'base/room.html')
+
+
+def getToken(request):
+    appId = "c1cb7dbad6bb4f5dba082f5ddc738405"
+    appCertificate = "1577c5a855854a7b9510a702668b8688"
+    channelName = request.GET.get('channel')
+    uid = random.randint(1, 230)
+    expirationTimeInSeconds = 3600
+    currentTimeStamp = int(time.time())
+    privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds
+    role = 1
+
+    token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
+
+    return JsonResponse({'token': token, 'uid': uid}, safe=False)
+
+
+@csrf_exempt
+def createMember(request):
+    data = json.loads(request.body)
+    member, created = RoomMember.objects.get_or_create(
+        name=data['name'],
+        uid=data['UID'],
+        room_name=data['room_name']
+    )
+
+    return JsonResponse({'name':data['name']}, safe=False)
+
+
+def getMember(request):
+    uid = request.GET.get('UID')
+    room_name = request.GET.get('room_name')
+
+    member = RoomMember.objects.get(
+        uid=uid,
+        room_name=room_name,
+    )
+    name = member.name
+    return JsonResponse({'name':member.name}, safe=False)
+
+@csrf_exempt
+def deleteMember(request):
+    data = json.loads(request.body)
+    member = RoomMember.objects.get(
+        name=data['name'],
+        uid=data['UID'],
+        room_name=data['room_name']
+    )
+    member.delete()
+    return JsonResponse('Member deleted', safe=False)
---
+++ readme.md
@@ -0,0 +1,45 @@
+# Agora Token Generator
+
+## Description 
+A Groupd video calling application using the Agora Web SDK with a Django backend.
+
+##  How to use this source code
+
+#### 1 - Clone repo
+```
+git clone https://github.com/divanov11/mychat
+```
+
+#### 2 - Install requirements
+```
+cd mychat
+pip install -r requirements.txt
+```
+
+#### 3 - Update Agora credentals
+In order to use this project you will need to replace the agora credentials in `veiws.py` and `streams.js`.
+
+Create an account at agora.io and create an `app`. Once you create your app, you will want to copy the `appid` & `appCertificate` to update `views.py` and `streams.js`. If you have questions about where to get your app id id recomend referenceing this link `https://youtu.be/HX6AM_1-jNM?t=88`
+
+###### views.py
+```
+def getToken(request):
+    appId = "YOUR APP ID"
+    appCertificate = "YOUR APPS CERTIFICATE"
+    ......
+```
+
+###### streams.js
+```
+....
+const APP_ID = 'YOUR APP ID'
+....
+```
+
+
+#### 4 - Start server
+```
+python manage.py runserver
+```
+
+
